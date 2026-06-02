18c5debe708f51108e3f1bc551c253968e2648d1
Aditya Raj <adityaraj93107@gmail.com>
2026-06-02 18:45:55 +0530

    connect navbar, home page and theme system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Navbar } from './components/layout/Navbar'
+import { Home } from "./pages/Home"
+import { useTheme } from './hooks/useTheme'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+function App() {
+    const { theme, toggleTheme } = useTheme()
+    return (
+        <div className="min-h-screen bg-zinc-950 text-zinc-100">
+            <Navbar 
+                theme={theme} 
+                onToggleTheme={toggleTheme} 
+            />
+            <Home />
+        </div>
+    )
 }
 
 export default App
