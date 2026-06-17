cd9b363af06fb0845bd54e934f1beac053874d63
Aditya Raj <adityaraj93107@gmail.com>
2026-06-17 14:43:43 +0530

    Add PageShell layout component

---
+++ src/components/layout/PageShell.tsx
@@ -0,0 +1,31 @@
+import { cn } from "../../lib/utils"
+import { useAppContext } from "../../context/AppContext"
+import { Navbar } from "./Navbar"
+import { Sidebar } from "./Sidebar"
+
+interface PageShellProps {
+    children: React.ReactNode  // whatever the page puts between the tags
+    className?: string
+}
+
+export function PageShell({ children, className }: PageShellProps) {
+    const { state } = useAppContext()   // to know current Sidebar state
+
+    return (
+        <div className="min-h-screen bg-zinc-950 text-zinc-100">
+            <Navbar />
+            <Sidebar />
+
+            <main
+                className={cn(
+                    "pt-14", 
+                    "transition-[margin-left] duration-300 ease-in-out",
+                    state.sidebarOpen ? "md:ml-72" : "ml-0",
+                    className
+                )}
+            >
+                {children}
+            </main>
+        </div>
+    )
+}
